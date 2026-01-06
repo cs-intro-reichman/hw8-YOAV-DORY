1ebfd23437eedb4373037f317a02efd109fd4a00
YOAV-DORY <yoav.dory@post.runi.ac.il>
2026-01-06 19:25:47 +0200

    commit

--- Network.java
+++ Network.java
@@ -122,7 +122,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
         
         for(int i = 0; i < users.length; i++){
             if(users[i] != null) ans = ans + "\n" + users[i].toString();    
